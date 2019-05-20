2a581cf32114733e10e29fc1ef7077be25ccacea
aliffrhn <alif.farhanw@gmail.com>
2019-05-21 04:28:08 +0700

    create dBestDesign Component

---
+++ Container/detailBestdesign.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { View, Image, ScrollView } from 'react-native'
+
+export default class DetailBestdesign extends Component{
+
+    render(){
+
+        return(
+            <ScrollView>
+                TODO: dbDesign
+            </ScrollView>
+        )
+    }
+}
